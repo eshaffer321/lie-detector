335bc68d779714ec32a8d492d1e7e1cef748158c
Erick Shaffer <erickshaffer@Ericks-MacBook-Pro.local>
2018-11-03 14:44:40 -0700

    added set of hostnames

--- background.js
+++ background.js
@@ -12,3 +12,12 @@ chrome.runtime.onMessage.addListener(
         if (request.greeting == "hello")
             sendResponse({farewell: "goodbye"});
     });
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://raw.githubusercontent.com/BigMcLargeHuge/opensources/master/sources/sources.json", false);
+xhr.send();
+
+var result = xhr.responseText;
+
+console.log(result);
--- content.js
+++ content.js
@@ -6,7 +6,17 @@ chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
 
 let ref = document.querySelectorAll('a');
 
-for (let i = 0; i < ref.length; i++) {
-    console.log(ref[i].href);
+const getLocation = function (href) {
+    const l = document.createElement('a');
+    l.href = href;
+    return l.host;
+};
+var cat = new Set();
 
+for (let i = 0; i < ref.length; i++) {
+    //console.log(ref[i]);
+    console.log(getLocation(ref[i].href));
+    cat.add(getLocation(ref[i].href));
 }
+
+console.log(cat);
