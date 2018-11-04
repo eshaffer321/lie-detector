448a6010ef58f631775d166773e8d1642e5d6676
Jason Henderson <jason@mbay.io>
2018-11-03 23:00:14 -0700

    basic working extension fixes

--- popup.js
+++ popup.js
@@ -1,33 +1,37 @@
 function awesome() {
-    // Do something awesome!
+  // Do something awesome!
+  $('.chart').easyPieChart({
+    //your options goes here
+  });
 }
 
 function totallyAwesome() {
-    // do something TOTALLY awesome!
+  // do something TOTALLY awesome!
 }
 
 function awesomeTask() {
-    awesome();
-    totallyAwesome();
+  awesome();
+  totallyAwesome();
 }
 
 function clickHandler(e) {
-    setTimeout(awesomeTask, 1000);
+  setTimeout(awesomeTask, 1000);
 }
 
 function main() {
-    console.log("asdf");  
-    var element = document.querySelector('.chart');
-    new EasyPieChart(element, {
-        // your options goes here
-    });  
+  // $('.chart').easyPieChart({
+  //   //your options goes here
+  // });
 }
 
 // Add event listeners once the DOM has fully loaded by listening for the
 // `DOMContentLoaded` event on the document, and adding your listeners to
 // specific elements when it triggers.
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('button').addEventListener('click', clickHandler);
-    main();
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.querySelector('button').addEventListener('click', clickHandler);
+//   main();
+// });
 
+$(function() {
+  $('button').click(clickHandler);
 });
