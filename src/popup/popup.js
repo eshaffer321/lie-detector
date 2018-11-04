3c83334e6ef7a68a41879a8abc9d1c5b1497208b
Erick Shaffer <erickshaffer@Ericks-MacBook-Pro.local>
2018-11-04 11:32:25 -0800

    changed styling

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,42 +1,13 @@
-// let changeColor = document.getElementById('changeColor');
-//
-// chrome.storage.sync.get('color', function (data) {
-//     changeColor.style.backgroundColor = data.color;
-//     changeColor.setAttribute('value', data.color);
-// });
-// changeColor.onclick = function (element) {
-//     console.log('im getting clicked');
-//     let color = element.target.value;
-//     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-//         chrome.tabs.executeScript(
-//             tabs[0].id,
-//             {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//     });
-// };
-//
-// document.addEventListener('DOMContentLoaded', function() {
-//   console.log('hi');
-// });
-//
-// chrome.runtime.onMessage.addListener(function (z, sender) {
-//     console.log(JSON.parse(z));
-//
-// });
 
-function awesome() {
+chrome.runtime.onMessage.addListener(function (z, sender) {
+    console.log('in pop up');
+    console.log(JSON.parse(z));
+    awesome(JSON.parse(z));
+});
+
+function awesome(z) {
     // Do something awesome!
-    $('.chart').easyPieChart({
-        //your options goes here
-    });
-}
-
-function totallyAwesome() {
-    // do something TOTALLY awesome!
-}
-
-function awesomeTask() {
-    awesome();
-    totallyAwesome();
+    $('.chart').data('easyPieChart').update(z);
 }
 
 function clickHandler(e) {
